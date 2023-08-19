a772058cfcc26ef4010c73e68de56009ed16160f
morasa zadbagher sighalani <100466704+MORASAzb@users.noreply.github.com>
2023-08-19 12:20:55 +0330

    Add files via upload

--- index1.js
+++ index1.js
@@ -1,8 +1,9 @@
 const firstName = prompt("لطفا نام خود را وارد کنید:");
 const lastName = prompt("لطفا نام خانوادگی خود را وارد کنید:");
+const removeSpaces = firstName => firstName.replace(/\s/g, '');
+const removeSpaces1 = lastName => lastName.replace(/\s/g, '');
 
-const fullName = firstName + lastName;
-const fullNameLength = fullName.length;
-
-console.log(`نام کامل شما: ${fullName}`);
-console.log(`طول مجموع نام و نام خانوادگی: ${fullNameLength}`);
+const fullName = removeSpaces(firstName).length + removeSpaces1(lastName).length;
+/*const fullNameLength = fullName.length;
+console.log(`نام کامل شما: ${fullName}`);*/
+console.log(`طول مجموع نام و نام خانوادگی: ${fullName }`);
